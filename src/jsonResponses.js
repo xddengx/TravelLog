3ec3f8d703c3060c01038ef379f17f2b530463cc
Dina Deng <dxd4832@g.rit.edu>
2018-10-01 17:30:01 -0400

    Add log (post) and get all logs working. Not stylized yet.

---
+++ src/jsonResponses.js
@@ -0,0 +1,96 @@
+const logs = {};
+
+const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  // sending response with json object
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// repsonse without a message/json body
+const respondJSONMeta = (request, response, status) => {
+  response.writeHead(status, { 'Content-Type': 'application/json' });
+  response.end();
+};
+
+// 200 - successful
+const getLog = (request, response) => {
+  const jsonResponse = {
+    logs,
+  };
+
+  return respondJSON(request, response, 200, jsonResponse);
+};
+
+const getLogMeta = (request, response) => {
+  respondJSON(request, response, 200);
+};
+
+// POST
+const addLog = (request, response, body) => {
+  const jsonResponse = {
+    message: 'Travel Start, End, and Destination are required.',
+  };
+
+  // validation, 400 - bad request
+  if (!body.startDate || !body.endDate || !body.destination) {
+    jsonResponse.id = 'missingParams';
+    return respondJSON(request, response, 400, jsonResponse);
+  }
+
+  let responseCode = 201;
+
+  //&& logs[body.endDate] && logs[body.destination]
+  if (logs[body.destination]) {
+    responseCode = 204;
+  } else {
+    // create new log
+    logs[body.destination] = {};
+  }
+
+  // adding/updating logs
+  logs[body.destination].startDate = body.startDate;
+  logs[body.destination].endDate = body.endDate;
+  logs[body.destination].destination = body.destination;
+  logs[body.destination].carrier = body.carrier;
+  logs[body.destination].currency = body.currency;
+  logs[body.destination].expenses = body.expenses;
+  logs[body.destination].sites = body.sites;
+
+//   console.dir(logs[body.startDate]);
+
+  if (responseCode === 201) {
+    jsonResponse.message = 'Log Created Successfully';
+    return respondJSON(request, response, responseCode, jsonResponse);
+  }
+
+  // 204 success without message
+  return respondJSONMeta(request, response, responseCode);
+};
+
+// 404 error message
+const notFound = (request, response) => {
+  const jsonResponse = {
+    id: 'notFound',
+    message: 'The page you are looking for is not found',
+  };
+
+  respondJSON(request, response, 404, jsonResponse);
+};
+
+// 404 no error message
+const notFoundMeta = (request, response) => {
+  respondJSONMeta(request, response, 404);
+};
+
+module.exports = {
+  getLog,
+  getLogMeta,
+  addLog,
+  notFound,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,101 @@
+const http = require('http'); // http module
+const url = require('url'); // url module
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// GET
+const urlStructGET = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/allLogs': jsonHandler.getLog,
+  '/notReal': jsonHandler.notFound,
+  notFound: jsonHandler.notFound,
+};
+
+// HEAD
+const urlStructHEAD = {
+  '/getLogs': jsonHandler.getLogMeta,
+  '/notReal': jsonHandler.notFoundMeta,
+  notFound: jsonHandler.notFoundMeta,
+};
+
+// POST
+const handlePost = (request, response, parsedUrl) => {
+  // if post is to /addLog (our only POST url)
+  if (parsedUrl.pathname === '/addLog') {
+    const res = response;
+
+    // uploads come in as a byte stream that we need
+    // to reassemble once it's all arrived
+    const body = [];
+
+    // if the upload stream errors out, just throw a
+    // a bad request and send it back
+    request.on('error', (err) => {
+      console.dir(err);
+      res.statusCode = 400;
+      res.end();
+    });
+
+    // on 'data' is for each byte of data that comes in
+    // from the upload. We will add it to our byte array.
+    request.on('data', (chunk) => {
+      body.push(chunk);
+    });
+
+    // on end of upload stream.
+    request.on('end', () => {
+      // combine our byte array (using Buffer.concat)
+      // and convert it to a string value (in this instance)
+      const bodyString = Buffer.concat(body).toString();
+      // since we are getting x-www-form-urlencoded data
+      // the format will be the same as querystrings
+      // Parse the string into an object by field name
+      const bodyParams = query.parse(bodyString);
+      // console.dir(bodyParams);
+
+      // pass to our addLog function
+      jsonHandler.addLog(request, res, bodyParams);
+    });
+  }
+};
+
+const onRequest = (request, response) => {
+  // parse the url using the url module
+  const parsedUrl = url.parse(request.url);
+
+  // check if the path name (the /name part of the url) matches
+  // any in our url object. If so call that function. If not, default to index.
+  switch (request.method) {
+    case 'GET':
+      if (urlStructGET[parsedUrl.pathname]) {
+        urlStructGET[parsedUrl.pathname](request, response);
+      } else {
+        urlStructGET.notFound(request, response);
+      }
+      break;
+
+    case 'HEAD':
+      if (urlStructHEAD[parsedUrl.pathname]) {
+        urlStructHEAD[parsedUrl.pathname](request, response);
+      } else {
+        urlStructHEAD.notFoundMeta(request, response);
+      }
+      break;
+
+    case 'POST':
+      handlePost(request, response, parsedUrl);
+      break;
+
+    default:
+      jsonHandler.notFound(request, response);
+  }
+};
+
+// start server
+http.createServer(onRequest).listen(port);
+
+console.log(`Listening on 127.0.0.1: ${port}`);
